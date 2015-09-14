c9983e34fd67a812ca76a4863927db33fdc2ed7e
lokesh <lokesh.surage@thetechchefs.in>
2015-09-14 23:56:41 +0530

    call

--- app/src/main/java/cool/app/mcommerce/techchefs/activitylifecycle/MainActivity.java
+++ app/src/main/java/cool/app/mcommerce/techchefs/activitylifecycle/MainActivity.java
@@ -1,38 +1,59 @@
 package cool.app.mcommerce.techchefs.activitylifecycle;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.CharacterPickerDialog;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                builder1.setMessage("Write your message here.");
+                builder1.setCancelable(true);
+                builder1.setPositiveButton("Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                builder1.setNegativeButton("No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
             }
         });
+
+        Log.d("onCreate", "onCreate");
+
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d("Onstart", "onstart");
 
 
     }
@@ -40,40 +61,48 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("onResume", "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d("onPause", "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.d("onStop", "onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.d("onRestart", "onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d("onDestroy", "onDestroy");
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        Log.d("onBackPressed", "onBackPressed");
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
+        Log.d("onSaveInstanceState", "onSaveInstanceState");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        Log.d("onRestoreInstanceState", "onRestoreInstanceState");
     }
 }
